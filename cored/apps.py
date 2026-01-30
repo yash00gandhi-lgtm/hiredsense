355dd5295e5bf5101084a32c1cee9f817c986a8b
yash00gandhi-lgtm <yash00gandhi@gmail.com>
2026-01-30 11:24:45 +0530

    Create superuser safely using post_migrate signal

--- cored/apps.py
+++ cored/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 from django.contrib.auth import get_user_model
+from django.db.models.signals import post_migrate
 import os
 
 class CoredConfig(AppConfig):
@@ -7,16 +8,22 @@ class CoredConfig(AppConfig):
     name = "cored"
 
     def ready(self):
-        User = get_user_model()
+        post_migrate.connect(create_superuser, sender=self)
 
-        username = os.getenv("DJANGO_SUPERUSER_USERNAME")
-        email = os.getenv("DJANGO_SUPERUSER_EMAIL")
-        password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
 
-        if username and password:
-            if not User.objects.filter(username=username).exists():
-                User.objects.create_superuser(
-                    username=username,
-                    email=email,
-                    password=password
-                )
+def create_superuser(sender, **kwargs):
+    User = get_user_model()
+
+    username = os.getenv("DJANGO_SUPERUSER_USERNAME")
+    email = os.getenv("DJANGO_SUPERUSER_EMAIL")
+    password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
+
+    if not username or not password:
+        return
+
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(
+            username=username,
+            email=email,
+            password=password
+        )
