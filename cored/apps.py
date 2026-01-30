4aec601430c5b9f10095423dc147205d9020c127
yash00gandhi-lgtm <yash00gandhi@gmail.com>
2026-01-30 11:15:09 +0530

    Auto-create superuser on startup using env variables

--- cored/apps.py
+++ cored/apps.py
@@ -1,6 +1,22 @@
 from django.apps import AppConfig
-
+from django.contrib.auth import get_user_model
+import os
 
 class CoredConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cored'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "cored"
+
+    def ready(self):
+        User = get_user_model()
+
+        username = os.getenv("DJANGO_SUPERUSER_USERNAME")
+        email = os.getenv("DJANGO_SUPERUSER_EMAIL")
+        password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
+
+        if username and password:
+            if not User.objects.filter(username=username).exists():
+                User.objects.create_superuser(
+                    username=username,
+                    email=email,
+                    password=password
+                )
